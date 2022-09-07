ad396b52ba20a753b5e468c07391c7a87493ff0d
[Bashar BaniMelhem] <[basharbanimelhem@gmail.com]>
2022-09-08 01:13:36 +0300

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyApGl2h7n-84mecSs0QjB-6gRgchks6hTQ",
+    authDomain: "upscale-by-volunteering.firebaseapp.com",
+    projectId: "upscale-by-volunteering",
+    storageBucket: "upscale-by-volunteering.appspot.com",
+    messagingSenderId: "136814633655",
+    appId: "1:136814633655:web:da5c22e30d7ba50c50e332",
+    measurementId: "G-DR2GER9P8T"
+  }
 };
